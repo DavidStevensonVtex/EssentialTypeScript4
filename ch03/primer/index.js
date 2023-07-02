6e118661b9ed5bc52daba8c7c37d486ad87167a3
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 15:20:56 -0400

    Values have Types, not variables

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -12,7 +12,17 @@ if (hatPrice == bootsPrice) {
 let totalPrice = hatPrice + bootsPrice ;
 console.log(`Total Price: ${totalPrice}`);
 
+// Values have types, not variables.
+let myVariable = "Adam" ;
+console.log(`Type: ${typeof myVariable}`);
+
+myVariable = 100 ;
+console.log(`Type: ${typeof myVariable}`);
+
+
 // Hat price: 100
 // Boots price: 100
 //   Prices are the same
 //   Total Price: 100100
+//   Type: string
+//   Type: number
