ec86e395a381213bb31f7bafbaf8f2a2298c32c2
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 16:22:23 -0400

    Destructuring Arrays

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -1,26 +1,4 @@
 let names = [ "Hat", "Boots", "Gloves" ];
-let prices = [] ;
-
-prices.push(100);
-prices.push("100");
-prices.push(50.25);
-
-console.log(`First Item: ${names[0]}: ${prices[0]}`);
-
-let sumPrices = (...numbers) => numbers.reduce((total, val) =>
-        total + (Number.isNaN(Number(val)) ? 0 : Number(val))) ;
-
-let totalPrice = sumPrices(...prices);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let combinedArray = [...names, ...prices] ;
-combinedArray.forEach(element => console.log(`Combined Array Element: ${element}`));
-
-// First Item: Hat: 100
-// Total: 250.25 number
-// Combined Array Element: Hat
-// Combined Array Element: Boots
-// Combined Array Element: Gloves
-// Combined Array Element: 100
-// Combined Array Element: 100
-// Combined Array Element: 50.25
+let [one, two] = names ;
+console.log(`One: ${one}, Two: ${two}`);
+// One: Hat, Two: Boots
