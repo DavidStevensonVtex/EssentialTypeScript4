c7f72e930dae59b951a068ba5a116cfd065cbc17
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 17:16:42 -0400

    Understanding the this Keyword in Stand-Alone Functions

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -1,39 +1,11 @@
-let hat = {
-    name: "Hat",
-    _price: 100,
-    priceIncTax: 100 * 1.2,
+function writeMessage(message) {
+    console.log(`${this.greeting}, ${message}`);
+}
 
-    set price(newPrice) {
-        this._price = newPrice ;
-        this.priceIncTax = this._price * 1.2 ;
-    },
+greeting = "Hello" ;
 
-    get price() {
-        return this._price ;
-    },
+writeMessage("It is sunny today") ;
+writeMessage.call(global, "It is sunny today");
 
-    writeDetails : () => 
-        console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`)
-} ;
-
-let boots = {
-    name: "Boots",
-    price: "100",
-
-    get priceIncTax() {
-        return Number(this.price) * 1.2 ;
-    }
-} ;
-
-hat.writeDetails();
-hat.price = 120 ;
-hat.writeDetails();
-
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
-boots.price = "120" ;
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
-
-// undefined: undefined, undefined
-// undefined: undefined, undefined
-// Boots: 100, 120
-// Boots: 120, 144
+// Hello, It is sunny today
+// Hello, It is sunny today
