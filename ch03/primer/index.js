f9cd05cfbe940703b863e30e857dae6ff6ecc6a3
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 16:00:57 -0400

    Replacing a function with an Arrow Function

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -3,11 +3,8 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100" ;
 console.group(`Boots price: ${bootsPrice}`);
 
-function sumPrices(...numbers)
-{
-    return numbers.reduce((total, val) =>
+let sumPrices = (...numbers) => numbers.reduce((total, val) =>
         total + (Number.isNaN(Number(val)) ? 0 : Number(val))) ;
-}
 
 let totalPrice = sumPrices(hatPrice, bootsPrice);
 console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
