e9283c10974f11ddc7b536a2be03dbe1c4fd8cf4
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 17:21:48 -0400

    Understanding this in Methods

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -1,11 +1,20 @@
-function writeMessage(message) {
-    console.log(`${this.greeting}, ${message}`);
+let myObject = {
+    greeting: "Hi, there",
+
+    writeMessage(message) {
+        console.log(`${this.greeting}, ${message}`);
+    }
 }
 
 greeting = "Hello" ;
 
-writeMessage("It is sunny today") ;
-writeMessage.call(global, "It is sunny today");
+myObject.writeMessage("It is sunny today") ;
+myObject.writeMessage.call(myObject, "It is sunny today") ;
+// Hi, there, It is sunny today
+// Hi, there, It is sunny today
 
+let myFunction = myObject.writeMessage ;
+myFunction("It is sunny today");
+myFunction.call(myObject, "It is sunny today");
 // Hello, It is sunny today
-// Hello, It is sunny today
+// Hi, there, It is sunny today
