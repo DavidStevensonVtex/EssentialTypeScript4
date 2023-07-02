a11d8ed32952ea3a288f7aeb43a37512979c1406
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 15:10:41 -0400

    Getting Confused by JavaScript
    The Total Price value of 100100 reflects string concatenation rather than the expected number addition.

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -2,3 +2,17 @@ let hatPrice = 100 ;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100" ;
 console.group(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+    console.log("Prices are the same");
+} else {
+    console.log("Prices are different");
+}
+
+let totalPrice = hatPrice + bootsPrice ;
+console.log(`Total Price: ${totalPrice}`);
+
+// Hat price: 100
+// Boots price: 100
+//   Prices are the same
+//   Total Price: 100100
