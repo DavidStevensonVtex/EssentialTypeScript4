cd7042aeb0ec6d9e59388af43ccb0d116ab4dd80
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 15:44:36 -0400

    Working with Function Results

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -3,34 +3,19 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100" ;
 console.group(`Boots price: ${bootsPrice}`);
 
-if (hatPrice === bootsPrice) {
-    console.log("Prices are the same");
-} else {
-    console.log("Prices are different");
+function sumPrices(first, second, third)
+{
+    return first + second + third ;
 }
 
-let totalPrice = Number(hatPrice) + Number(bootsPrice) ;
-console.log(`Total Price: ${totalPrice}`);
+let totalPrice = sumPrices(hatPrice, bootsPrice);
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+// Total Price: 100100undefined
 
-// Values have types, not variables.
-let myVariable = "Adam" ;
-console.log(`Type: ${typeof myVariable}`);
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+// Total Price: 600 number
 
-myVariable = 100 ;
-console.log(`Type: ${typeof myVariable}`);
-
-let firstCity ;
-let secondCity = firstCity || "London" ;
-console.log(`City: ${secondCity}`);
-// City: London
-
-// Understanding Nullish Coalescing
-// 0, null and NaN are falsy values.
-// ?? onlycoerces undefined and null values, and not the other falsy values, in this example.
-let taxRate ;       // no tax rate has been defined
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-taxRate = 0;        // zero-rated for tax
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-// Tax rate: 10%
-// Tax rate: 0%
-
+totalPrice = sumPrices(100,200) ;
+console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+//   Total Price: NaN number
