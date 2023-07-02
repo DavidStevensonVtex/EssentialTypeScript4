625440ed1ae5092afab62432b9f8158badac055e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 16:30:46 -0400

    Assigning Remaining Elements to an Array

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -3,3 +3,7 @@ let names = [ "Hat", "Boots", "Gloves" ];
 let [, , three] = names ;
 console.log(`Three: ${three}`);
 // Three: Gloves
+
+let prices = [ 100, 120, 50.25 ] ;
+let [, ...highest] = prices.sort((a, b) => a - b);
+highest.forEach(price => console.log(`High price: ${price}`));
