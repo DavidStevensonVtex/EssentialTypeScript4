5227cf1872113409e25ef1b95bf87c16c6e1538a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 15:23:13 -0400

    Avoiding Unintentional Type Coercion
    === is the strict equality operator
    Number(value) converts a value to a Number (if possible)

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -3,13 +3,13 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100" ;
 console.group(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
     console.log("Prices are the same");
 } else {
     console.log("Prices are different");
 }
 
-let totalPrice = hatPrice + bootsPrice ;
+let totalPrice = Number(hatPrice) + Number(bootsPrice) ;
 console.log(`Total Price: ${totalPrice}`);
 
 // Values have types, not variables.
@@ -19,10 +19,9 @@ console.log(`Type: ${typeof myVariable}`);
 myVariable = 100 ;
 console.log(`Type: ${typeof myVariable}`);
 
-
 // Hat price: 100
 // Boots price: 100
-//   Prices are the same
-//   Total Price: 100100
+//   Prices are different
+//   Total Price: 200
 //   Type: string
 //   Type: number
