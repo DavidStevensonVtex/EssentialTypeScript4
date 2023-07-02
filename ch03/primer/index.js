f8f344f7ce19a656b08bbcd1c21faa2089063f98
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 16:23:37 -0400

    Ignoring Elements When Destructuring an Array

--- ch03/primer/index.js
+++ ch03/primer/index.js
@@ -1,4 +1,5 @@
 let names = [ "Hat", "Boots", "Gloves" ];
-let [one, two] = names ;
-console.log(`One: ${one}, Two: ${two}`);
-// One: Hat, Two: Boots
+
+let [, , three] = names ;
+console.log(`Three: ${three}`);
+// Three: Gloves
