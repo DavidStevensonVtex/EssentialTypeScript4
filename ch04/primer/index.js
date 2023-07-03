c0300be42d8e73db54aa51677ab35f27ba94b31d
David Stevenson <drstevenson1958@yahoo.com>
2023-07-03 12:00:27 -0400

    Defining Static Properties and Methods

--- ch04/primer/index.js
+++ ch04/primer/index.js
@@ -7,34 +7,10 @@ Product.prototype.toString = function() {
     return `toString: Name: ${this.name}, Price: ${this.price}`;
 }
 
-let TaxedProduct = function ( name, price, taxRate ) {
-    Product.call(this, name, price) ;
-    this.taxRate = taxRate ;
-}
+Product.process = (...products) => 
+    products.forEach(p => console.log(p.toString()));
 
-Object.setPrototypeOf(TaxedProduct.prototype, Product.prototype);
+Product.process ( new Product("Hat", 100, 1.2), new Product("Boots", 100));
 
-TaxedProduct.prototype.getPriceIncTax = function() {
-    return Number(this.price) * this.taxRate ;
-}
-
-TaxedProduct.prototype.toTaxString = function() {
-    return `${this.toString()}, Tax: ${this.getPriceIncTax()}` ;
-}
-
-let hat = new TaxedProduct("Hat", 100, 1.2);
-let boots = new Product("Boots", 100) ;
-
-console.log(hat.toTaxString());
-console.log(boots.toString());
-console.log(`hat instanceof TaxedProduct:   ${ hat instanceof TaxedProduct}`);
-console.log(`hat instanceof Product:        ${ hat instanceof Product}`);
-console.log(`boots instanceof TaxedProduct: ${ boots instanceof TaxedProduct}`);
-console.log(`boots instanceof Product:      ${ boots instanceof Product}`);
-
-// toString: Name: Hat, Price: 100, Tax: 120
+// toString: Name: Hat, Price: 100
 // toString: Name: Boots, Price: 100
-// hat instanceof TaxedProduct:   true
-// hat instanceof Product:        true
-// boots instanceof TaxedProduct: false
-// boots instanceof Product:      true
