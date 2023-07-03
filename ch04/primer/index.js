0bfa2bfdb0cc0784d17fd82961ed5034b926f240
David Stevenson <drstevenson1958@yahoo.com>
2023-07-03 11:48:32 -0400

    Chaining Constructor Functions

--- ch04/primer/index.js
+++ ch04/primer/index.js
@@ -7,11 +7,26 @@ Product.prototype.toString = function() {
     return `toString: Name: ${this.name}, Price: ${this.price}`;
 }
 
-let hat = new Product("Hat", 100);
-let boots = new Product("Boots", 100) ;
+let TaxedProduct = function ( name, price, taxRate ) {
+    Product.call(this, name, price) ;
+    this.taxRate = taxRate ;
+}
 
-console.log(hat.toString());
+Object.setPrototypeOf(TaxedProduct.prototype, Product.prototype);
+
+TaxedProduct.prototype.getPriceIncTax = function() {
+    return Number(this.price) * this.taxRate ;
+}
+
+TaxedProduct.prototype.toTaxString = function() {
+    return `${this.toString()}, Tax: ${this.getPriceIncTax()}` ;
+}
+
+let hat = new TaxedProduct("Hat", 100, 1.2);
+let boots = new TaxedProduct("Boots", 100, 1.2) ;
+
+console.log(hat.toTaxString());
 console.log(boots.toString());
 
-// toString: Name: Hat, Price: 100
+// toString: Name: Hat, Price: 100, Tax: 120
 // toString: Name: Boots, Price: 100
