ba1972044335ae747dea73ca72d508cafbcb598e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 14:34:36 -0400

    Listing 4-20: Using a Map

---
+++ ch04/primer/listing_4_20_using_a_map.js
@@ -0,0 +1,19 @@
+class Product {
+    constructor(name, price) {
+        this.name = name ;
+        this.price = price ;
+    }
+
+    toString() {
+        return `toString: Name: ${this.name}, Price: ${this.price}`;
+    }
+}
+
+let data = new Map() ;
+data.set("hat", new Product("Hat", 100));
+data.set("boots", new Product("Boots", 100));
+
+[...data.keys()].forEach(key => console.log(data.get(key).toString()));
+
+// toString: Name: Hat, Price: 100
+// toString: Name: Boots, Price: 100
