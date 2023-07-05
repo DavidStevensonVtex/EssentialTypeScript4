d13ff588192e6b618cf24c0eebdda0c6c64d3df1
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 15:25:23 -0400

    Exporting and Importing a Named Feature

--- ch04/primer/tax.js
+++ ch04/primer/tax.js
@@ -1,5 +1,5 @@
-// Listing 4-25
+// Listing 4-27: Exporting a Named Feature in the tax.js file
 
-export default function ( price ) {
+export function calculateTax ( price ) {
     return Number(price) * 1.2 ;
 }
