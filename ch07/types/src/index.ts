3232133f2b0e65b9107e3621fb564d8f485e3ecc
David Stevenson <drstevenson1958@yahoo.com>
2023-07-11 15:12:44 -0400

    Listing 7-21. Using a Type Union in the index.ts File in the src Folder

--- ch07/types/src/index.ts
+++ ch07/types/src/index.ts
@@ -1,16 +1,7 @@
-function calculateTax(amount) : any {
-    return `$${(amount * 1.2).toFixed(2)}` ;
+function calculateTax(amount : number, format: boolean) : string | number {
+    const calcAmount = amount * 1.2 ;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount ;
 }
 
-let price = 100 ;
-let taxAmount = calculateTax(price);
-let halfShare = taxAmount / 2 ;
-
-let personVal = calculateTax("Bob");
-
-console.log(`Price: ${price}`);
-console.log(`Full amount in tax: ${taxAmount}`);
-console.log(`Half share: ${halfShare}`);
-console.log(`Name: ${personVal}`);
-
-// src/index.ts(1,23): error TS7006: Parameter 'amount' implicitly has an 'any' type.
+let taxAmount = calculateTax(100, false);
+let taxString = calculateTax(100, true);
