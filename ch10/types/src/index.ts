c394acc1ff07b0c49226ae500650e508743bb640
David Stevenson <drstevenson1958@yahoo.com>
2023-07-14 15:45:06 -0400

    Listing 10-22. Adding Properties with Different Types in the index.ts File in the src Folder

--- ch10/types/src/index.ts
+++ ch10/types/src/index.ts
@@ -1,54 +1,17 @@
 type Person = {
     id:   string,
     name: string,
-    city: string
+    city: string,
+    contact: number
 } ;
 
 type Employee = {
     id: string,
     company: string,
-    dept: string
+    dept: string,
+    contact: string
 } ;
 
 type EmployedPerson = Person & Employee ;
 
-function correlateData(peopleData: Person[], staff: Employee[]): EmployedPerson[] {
-    const defaults = { company: "None", dept: "None" } ;
-    return peopleData.map(p => ( {...p, ...staff.find(e => e.id === p.id) || { ...defaults, id: p.id } })) ;
-}
-
-let people: Person[] = [
-    { id: "bsmith",  name: "Bob Smith",    city: "London" },
-    { id: "ajones",  name: "Alice Johnes", city: "Paris" },
-    { id: "dpeters", name: "Dora Peters",  city: "New York" }
-] ;
-
-let employees: Employee[] = [
-    { id: "bsmith", company: "Acme Co", dept: "Sales" },
-    { id: "dpeters", company: "Acme Co", dept: "Development" }
-] ;
-
-let dataItems: EmployedPerson[] = correlateData(people, employees) ;
-
-function writePerson(per: Person): void {
-    console.log(`Person:   ${per.id}, ${per.name},    ${per.city}`);
-}
-
-function writeEmployee(emp: Employee): void {
-    console.log(`Employee: ${emp.id}, ${emp.company}, ${emp.dept}`);
-}
-
-dataItems.forEach(item => {
-    console.log();
-    writePerson(item);
-    writeEmployee(item);
-});
-
-// Person:   bsmith, Bob Smith,    London     
-// Employee: bsmith, Acme Co, Sales
-
-// Person:   ajones, Alice Johnes,    Paris   
-// Employee: ajones, None, None
-
-// Person:   dpeters, Dora Peters,    New York
-// Employee: dpeters, Acme Co, Development    
+let typeTest = ({} as EmployedPerson).contact ;
