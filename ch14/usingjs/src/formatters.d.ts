423553567fa14d9d6d594aaaa1ea093c4815dcb8
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 15:09:06 -0400

    Listing 14-21. Adding a Statement in the formatters.d.ts File in the src Folder

--- ch14/usingjs/src/formatters.d.ts
+++ ch14/usingjs/src/formatters.d.ts
@@ -1,2 +1,3 @@
 export function sizeFormatter(thing: string, count: number ): void;
 export function costFormatter(thing: string, cost: number | string): void;
+export function writeMessage(message : string) : void ;
